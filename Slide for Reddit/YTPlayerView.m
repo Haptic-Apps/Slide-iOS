f736a9b69a7d677d49b857a242fd011d1ab871ee
ccrama <hapticappsdev@gmail.com>
2018-07-27 13:02:24 -0500

    Fixed YTPlayerHelper implementation

--- Slide for Reddit/YTPlayerView.m
+++ Slide for Reddit/YTPlayerView.m
@@ -708,16 +708,14 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
     [self addSubview:self.webView];
     
     NSError *error = nil;
-    NSString *path = [[NSBundle bundleForClass:[YTPlayerView class]] pathForResource:@"YTPlayerView-iframe-player"
-                                                                              ofType:@"html"
-                                                                         inDirectory:@"Assets"];
+    NSString *path = [[NSBundle bundleForClass:[YTPlayerView class]] pathForResource:@"YTPlayer"
+                                                                              ofType:@"html"];
     
     // in case of using Swift and embedded frameworks, resources included not in main bundle,
     // but in framework bundle
     if (!path) {
-        path = [[[self class] frameworkBundle] pathForResource:@"YTPlayerView-iframe-player"
-                                                        ofType:@"html"
-                                                   inDirectory:@"Assets"];
+        path = [[[self class] frameworkBundle] pathForResource:@"YTPlayer"
+                                                        ofType:@"html"];
     }
     
     NSString *embedHTMLTemplate =
@@ -855,14 +853,8 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
     webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
     webView.scrollView.scrollEnabled = NO;
     webView.scrollView.bounces = NO;
-    
-    if ([self.delegate respondsToSelector:@selector(playerViewPreferredWebViewBackgroundColor:)]) {
-        webView.backgroundColor = [self.delegate playerViewPreferredWebViewBackgroundColor:self];
-        if (webView.backgroundColor == [UIColor clearColor]) {
-            webView.opaque = NO;
-        }
-    }
-    
+    webView.backgroundColor = [UIColor clearColor];
+    webView.opaque = NO;
     return webView;
 }
 
