3145b07558b82e4bc3aae5507624c6c3a06aaaae
Jonathan Cole <joncole04412@gmail.com>
2018-08-03 23:02:23 -0400

    Fix transitions for image views

--- Slide for Reddit/PostContentPresentationController.swift
+++ Slide for Reddit/PostContentPresentationController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Haptic Apps. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class PostContentPresentationController: UIPresentationController {
@@ -41,54 +42,15 @@ class PostContentPresentationController: UIPresentationController {
                 return
             }
 
-            let storedTransform = embeddedVC.imageView.transform
-
-            // I'm so sorry
-
             let image = (sourceImageView as! UIImageView).image!
-//            embeddedVC.imageView.image = (sourceImageView as! UIImageView).image
-//            embeddedVC.view.layoutIfNeeded()
 
-//            let relative = vc.view.convert(sourceImageView.frame, to: embeddedVC.view)
-            let world1 = vc.view.convert(sourceImageView.bounds, from: sourceImageView)
-            let f = embeddedVC.scrollView.convert(embeddedVC.imageView.bounds, to: embeddedVC.view)
-//            let world2 = vc.view.convert(embeddedVC.imageView.bounds, from: embeddedVC.view)
+            let fromRect = vc.view.convert(sourceImageView.bounds, from: sourceImageView)
+            let inner = AVMakeRect(aspectRatio: embeddedVC.imageView.bounds.size, insideRect: embeddedVC.view.bounds)
+            let toRect = vc.view.convert(inner, from: embeddedVC.scrollView)
 
-            let widthScale = vc.view.bounds.size.width / image.size.width
-            let heightScale = vc.view.bounds.size.height / image.size.height
-            let minScale = min(widthScale, heightScale)
+            let newTransform = transformFromRect(from: toRect, toRect: fromRect)
 
-//            var origin = embeddedVC.scrollView.contentOffset
-//            var size = embeddedVC.scrollView.bounds.size
-//            let scale = CGFloat(1.0) / embeddedVC.scrollView.zoomScale
-//            if scale < CGFloat(1.0) {
-//                origin.x *= scale
-//                origin.y *= scale
-//                size.width *= scale
-//                size.height *= scale
-//            }
-//            let visibleRect = CGRect(origin: origin, size: size)
-
-            let inner = CGRect(x: 0, y: (embeddedVC.view.bounds.size.height / 2.0) - ((image.size.height / 2.0) * minScale), width: image.size.width * minScale, height: image.size.height * minScale)
-            let world2 = vc.view.convert(inner, from: embeddedVC.scrollView)
-
-            // OR: embeddedVC.scrollView.convert(embeddedVC.scrollView.bounds, to: embeddedVC.scrollView)
-
-            //let world2 = visibleRect
-//            let relative = sourceImageView.convert(sourceImageView.bounds, to: embeddedVC.scrollView)
-            //let relative = presentedView!.convert(sourceImageView.frame, from: embeddedVC.view)
-
-//            let newTransform = storedTransform.concatenating(CGAffineTransform(translationX: -1000, y: 0))//.concatenating(CGAffineTransform(rotationAngle: CGFloat.pi))
-//            let newTransform = transformFromRect(from: embeddedVC.imageView.frame, toRect: relative)
-
-            let newTransform = transformFromRect(from: world2, toRect: world1)
-
-//            let newTransform = transformFromRect(from: world1, toRect: <#T##CGRect#>)
-
-//            let stored3DTransform = embeddedVC.imageView.layer.transform
-//            let new3DTransform = CATransform3DMakeAffineTransform(newTransform)
-
-            embeddedVC.scrollView.transform = newTransform //storedTransform.concatenating(newTransform)
+            embeddedVC.scrollView.transform = embeddedVC.scrollView.transform.concatenating(newTransform)
             coordinator.animate(alongsideTransition: { _ in
                 embeddedVC.scrollView.transform = CGAffineTransform.identity
             })
