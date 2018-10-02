04eb29dff10daad1c71de9e551ec1e8760f1de12
ccrama <hapticappsdev@gmail.com>
2018-10-02 00:26:50 -0500

    Update version number

--- Slide for Reddit/AppDelegate.swift
+++ Slide for Reddit/AppDelegate.swift
@@ -176,7 +176,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 }
 
                 BioMetricAuthenticator.authenticateWithPasscode(reason: "Enter your password", cancelTitle: "Exit", success: {
-                    self?.backView!.isHidden = true
+                    self?.backView?.isHidden = true
                 }, failure: { (_) in
                     exit(0)
                 })
@@ -452,7 +452,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 backView?.backgroundColor = ColorUtil.backgroundColor
                 self.window?.addSubview(backView!)
             }
-            self.backView!.isHidden = false
+            self.backView?.isHidden = false
         }
         totalBackground = false
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -481,7 +481,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 backView?.backgroundColor = ColorUtil.backgroundColor
                 self.window?.addSubview(backView!)
             }
-            self.backView!.isHidden = true
+            self.backView?.isHidden = true
         }
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
