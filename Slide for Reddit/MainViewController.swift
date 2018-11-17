9c19ae53308ef02c9025913ffaf7a6c5d743bbd7
ccrama <hapticappsdev@gmail.com>
2018-11-16 18:10:07 -0600

    End editing when force expanding

--- Slide for Reddit/MainViewController.swift
+++ Slide for Reddit/MainViewController.swift
@@ -435,6 +435,8 @@ class MainViewController: ColorMuxPagingViewController, UINavigationControllerDe
         let height = view.frame.height
         let width = view.frame.width
         menuNav!.view.frame = CGRect(x: 0, y: self.view.frame.maxY - CGFloat(menuNav!.bottomOffset), width: width, height: height * 0.9)
+        menuNav!.expand()
+        menuNav!.view.endEditing(true)
         menuNav!.collapse()
     }
 
