6074d014a29f7384a85889b0eb2f0aeedb55b06d
ccrama <hapticappsdev@gmail.com>
2019-01-21 15:04:47 -0600

    Improvements to share extension handling

--- Open in Slide/ActionViewController.swift
+++ Open in Slide/ActionViewController.swift
@@ -21,12 +21,21 @@ class ActionViewController: UIViewController {
         // For example, look for an image and place it into an image view.
         // Replace this with something appropriate for the type[s] your extension supports.
         var urlFound = false
+        var count = 0
+        let length = self.extensionContext!.inputItems.count
         for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
+            count += 1
             for provider in item.attachments! {
                 if provider.hasItemConformingToTypeIdentifier("public.url") {
                     provider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, _) -> Void in
                         if let shareURL = url as? NSURL {
-                            print("Got URL!")
+                            let absolute = shareURL.absoluteString ?? ""
+                            
+                            if !absolute.matches(regex: "(?i)redd\\.it/\\w+") && !absolute.matches(regex: "(?i)reddit\\.com/[^/]*") {
+                                //Not a Reddit URL
+                                self.extensionContext!.cancelRequest(withError: NSError())
+                            }
+                            
                             urlFound = true
                             
                             var comps = URLComponents(url: shareURL as URL, resolvingAgainstBaseURL: false)!
@@ -40,8 +49,9 @@ class ActionViewController: UIViewController {
                             }
                         }
                     })
-
                     break
+                } else if count == length && !urlFound {
+                    self.extensionContext!.cancelRequest(withError: NSError())
                 }
             }
             
@@ -71,3 +81,29 @@ class ActionViewController: UIViewController {
     }
 
 }
+
+extension String {
+    func matches(regex: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [])
+            let results = regex.matches(in: self, options: [], range: NSRange(location: 0, length: self.length))
+            return results.count > 0
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return false
+        }
+    }
+    mutating func stringByRemovingRegexMatches(pattern: String, replaceWith: String = "") {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.caseInsensitive)
+            let range = NSRange(location: 0, length: self.length)
+            self = regex.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: replaceWith)
+        } catch {
+            return
+        }
+    }
+    var length: Int {
+        return self.count
+    }
+}
+
--- Slide for Reddit/RedditLink.swift
+++ Slide for Reddit/RedditLink.swift
@@ -263,5 +263,4 @@ extension String {
             return
         }
     }
-    
 }
