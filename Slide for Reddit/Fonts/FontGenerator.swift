6943113b5e2ce641b4de651591449212c4f26d07
Jonathan Cole <joncole04412@gmail.com>
2019-09-16 23:23:14 -0400

    Add ability to select San Francisco rounded and serif variants

--- Slide for Reddit/Fonts/FontGenerator.swift
+++ Slide for Reddit/Fonts/FontGenerator.swift
@@ -14,8 +14,8 @@ class FontGenerator {
     public static func fontOfSize(size: CGFloat, submission: Bool) -> UIFont {
         let fontName = UserDefaults.standard.string(forKey: submission ? "postfont" : "commentfont") ?? ( submission ? "AvenirNext-DemiBold" : "AvenirNext-Medium")
         let adjustedSize = size + CGFloat(submission ? SettingValues.postFontOffset : SettingValues.commentFontOffset)
-        let font = UIFont(name: fontName, size: adjustedSize) ?? UIFont.systemFont(ofSize: adjustedSize)
-        return font
+
+        return FontMapping.fromStoredName(name: fontName).font(ofSize: adjustedSize) ?? UIFont.systemFont(ofSize: adjustedSize)
     }
     
     public static func boldFontOfSize(size: CGFloat, submission: Bool) -> UIFont {
