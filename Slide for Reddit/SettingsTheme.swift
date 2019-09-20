701d36d1f3229a4a2de26c4d1aa523d10e99995a
James Downs <5879831+PiwwowPants@users.noreply.github.com>
2019-09-19 22:29:34 -0400

    refactor: resolved void return violations

--- Slide for Reddit/SettingsTheme.swift
+++ Slide for Reddit/SettingsTheme.swift
@@ -730,7 +730,7 @@ final public class PickerViewViewControllerColored: UIViewController {
     
     public typealias Values = [[String]]
     public typealias Index = (column: Int, row: Int)
-    public typealias Action = (_ vc: UIViewController, _ picker: UIPickerView, _ index: Index, _ values: Values) -> ()
+    public typealias Action = (_ vc: UIViewController, _ picker: UIPickerView, _ index: Index, _ values: Values) -> Void
     
     fileprivate var action: Action?
     fileprivate var values: Values = [[]]
