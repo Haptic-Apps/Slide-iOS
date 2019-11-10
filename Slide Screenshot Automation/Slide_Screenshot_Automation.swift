48bc23290203b77a70a15dc77773c213a6b52afc
ccrama <hapticappsdev@gmail.com>
2019-11-10 15:02:27 -0600

    (Big One) Added Fastlane into the Slide iOS project, Created schemes for screenshot automation (still need to set this up)

---
+++ Slide Screenshot Automation/Slide_Screenshot_Automation.swift
@@ -0,0 +1,44 @@
+//
+//  Slide_Screenshot_Automation.swift
+//  Slide Screenshot Automation
+//
+//  Created by Carlos Crane on 11/10/19.
+//  Copyright © 2019 Haptic Apps. All rights reserved.
+//
+
+import XCTest
+
+class Slide_Screenshot_Automation: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
