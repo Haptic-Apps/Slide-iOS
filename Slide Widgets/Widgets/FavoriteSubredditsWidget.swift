2230403ba0d44fd06947bcd45b6c465df3e0f929
Jonathan Cole <joncole04412@gmail.com>
2020-09-15 21:38:57 -0400

    Refactor widget code

---
+++ Slide Widgets/Widgets/FavoriteSubredditsWidget.swift
@@ -0,0 +1,172 @@
+//
+//  FavoriteSubredditsWidget.swift
+//  Slide Widgets
+//
+//  Created by Jonathan Cole on 9/15/20.
+//  Copyright © 2020 Haptic Apps. All rights reserved.
+//
+
+import Combine
+#if canImport(WidgetKit)
+import WidgetKit
+#endif
+import SwiftUI
+
+struct Favorite_Subreddits: Widget {
+    /*
+     Corresponds to USR_DOMAIN in info.plist, which derives its value
+     from USR_DOMAIN in the pbxproj build settings. Default is `ccrama.me`.
+     */
+     func USR_DOMAIN() -> String {
+        return Bundle.main.object(forInfoDictionaryKey: "USR_DOMAIN") as! String
+     }
+
+    let kind: String = "Favorite_Subreddits"
+
+    var body: some WidgetConfiguration {
+
+        IntentConfiguration(kind: kind, intent: TimelineSubredditIntent.self, provider: SubredditsProvider()) { entry in
+            Favorite_SubredditsEntryView(entry: entry)
+        }
+        .configurationDisplayName("Subreddit")
+        .description("Quick links to your favorite Subreddit.")
+    }
+}
+
+struct Favorite_SubredditsEntryView: View {
+    /*
+    Corresponds to USR_DOMAIN in info.plist, which derives its value
+    from USR_DOMAIN in the pbxproj build settings. Default is `ccrama.me`.
+    */
+    func USR_DOMAIN() -> String {
+       return Bundle.main.object(forInfoDictionaryKey: "USR_DOMAIN") as! String
+    }
+
+    var entry: SubredditsProvider.Entry
+    @Environment(\.widgetFamily) var widgetFamily
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    init(entry: SubredditsProvider.Entry) {
+        self.entry = entry
+    }
+
+    var body: some View {
+        VStack {
+            if widgetFamily != .systemSmall {
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], alignment: .leading) {
+                    ForEach(entry.subreddits, id: \.self) { key in
+                        SubredditView(imageData: entry.imageData[key] ?? Data(), title: key, small: true)
+                    }
+                }.padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+            } else {
+                SubredditView(imageData: entry.imageData[entry.subreddits[0]] ?? Data(), title: entry.subreddits[0])
+            }
+        }.frame(maxHeight: .infinity)
+        .background(Color(widgetFamily == .systemSmall ? UIImage(data: entry.imageData[entry.subreddits[0]] ?? Data())?.averageColor ?? getSchemeColor() : getSchemeColor())
+                .opacity(0.8))
+    }
+
+    func getSchemeColor() -> UIColor {
+        return colorScheme == .light ? .white : .black
+    }
+
+    func getImage(item: String) -> String {
+        let shared = UserDefaults(suiteName: "group.\(USR_DOMAIN()).redditslide.prefs")
+        return shared?.string(forKey: item.lowercased()) ?? ""
+    }
+
+    func getTitle(item: Int) -> String {
+        let shared = UserDefaults(suiteName: "group.\(USR_DOMAIN()).redditslide.prefs")
+        let subs = shared?.stringArray(forKey: "favorites") ?? [""]
+        return subs[item]
+    }
+}
+
+// MARK: - SwiftUI Previews
+
+struct Favorite_Subreddits_Previews: PreviewProvider {
+    /*
+    Corresponds to USR_DOMAIN in info.plist, which derives its value
+    from USR_DOMAIN in the pbxproj build settings. Default is `ccrama.me`.
+    */
+    static func USR_DOMAIN() -> String {
+       return Bundle.main.object(forInfoDictionaryKey: "USR_DOMAIN") as! String
+    }
+
+    static var previews: some View {
+        Favorite_SubredditsEntryView(entry: SubredditEntry(date: Date(), subreddits: ["all", "frontpage", "popular", "slide_ios"], imageData: getPlaceholderData(["all", "frontpage", "popular", "slide_ios"])))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+
+    static func getPlaceholderData(_ subs: [String]) -> [String: Data] {
+        let shared = UserDefaults(suiteName: "group.\(USR_DOMAIN()).redditslide.prefs")
+        var toReturn = [String: Data]()
+        for item in subs {
+            if let data = shared?.data(forKey: "raw\(item)") {
+                toReturn[item] = data
+            } else if let url = URL(string: shared?.string(forKey: item.lowercased()) ?? "") {
+                do {
+                    toReturn[item] = try Data(contentsOf: url)
+                } catch {
+                    if let data = shared?.data(forKey: "raw") {
+                        toReturn[item] = data
+                    } else {
+                        toReturn[item] = Data()
+                    }
+                }
+            } else {
+                if let data = shared?.data(forKey: "raw") {
+                    toReturn[item] = data
+                } else {
+                    toReturn[item] = Data()
+                }
+            }
+        }
+        return toReturn
+    }
+}
+
+// MARK: - Views
+
+struct SubredditView: View {
+    var imageData: Data
+    var title: String
+    var small = false
+
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    var body: some View {
+        if !small {
+            //Full size small widget
+            VStack {
+                Image(uiImage: UIImage(data: imageData) ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipShape(Circle())
+                    .clipped()
+                Text(self.title).font(.subheadline).bold().foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.8).padding(EdgeInsets(top: 4, leading: 0, bottom: 0, trailing: 0))
+            }.widgetURL(URL(string: "slide://www.reddit.com/r/\(title)")).frame(maxWidth: .infinity)
+        } else {
+            //Grid widget
+            HStack {
+                VStack(alignment: .leading) {
+                    Link(destination: URL(string: "slide://www.reddit.com/r/\(title)")!) {
+                        Image(uiImage: UIImage(data: imageData) ?? UIImage())
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .clipShape(Circle())
+                            .clipped()
+                        Text(self.title).font(.caption).foregroundColor(colorScheme == .light ? .primary : .white).padding(EdgeInsets(top: 4, leading: 4, bottom: 0, trailing: 0))
+                    }.alignmentGuide(.leading) { d in d[.leading] }
+                }.padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+                Spacer()
+            }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(UIImage(data: imageData)?.averageColor ?? getSchemeColor()).opacity(0.8).cornerRadius(15))
+        }
+    }
+
+    func getSchemeColor() -> UIColor {
+        return colorScheme == .light ? .white : .black
+    }
+}
---
+++ Slide Widgets/Widgets/HotPostsWidget.swift
@@ -0,0 +1,169 @@
+//
+//  HotPostsWidget.swift
+//  Slide Widgets
+//
+//  Created by Jonathan Cole on 9/15/20.
+//  Copyright © 2020 Haptic Apps. All rights reserved.
+//
+
+import Combine
+#if canImport(WidgetKit)
+import WidgetKit
+#endif
+import SwiftUI
+
+struct Hot_Posts: Widget {
+    let kind: String = "Hot_Posts"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: SingleSubredditIntent.self, provider: HotPostsProvider()) { entry in
+            Hot_PostsEntryView(entry: entry)
+        }
+        .configurationDisplayName("Hot Posts")
+        .description("Shows hot posts from a Subreddit.")
+    }
+}
+
+struct Hot_PostsEntryView: View {
+    var entry: HotPostsProvider.Entry
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @Environment(\.widgetFamily) var widgetFamily
+
+    var body: some View {
+        if entry.subreddit == "redacted" {
+            VStack(alignment: .leading) {
+                SubredditViewHorizontal(imageData: entry.imageData, title: entry.subreddit, redacted: true).padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                if !entry.posts.posts.isEmpty {
+                    ForEach((0..<min(entry.posts.posts.count, (widgetFamily == .systemMedium ? 2 : 5)))) { post in
+                        PostView(post: entry.posts.posts[post], redacted: true)
+                    }
+                }
+                Spacer()
+            }.frame(maxHeight: .infinity).background(Color(UIImage(data: entry.imageData)?.averageColor ?? getSchemeColor() )
+                                                        .opacity(0.8)).redacted(reason: .placeholder)
+        } else {
+            VStack(alignment: .leading) {
+                SubredditViewHorizontal(imageData: entry.imageData, title: entry.subreddit, redacted: false).padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                if !entry.posts.posts.isEmpty {
+                    ForEach((0..<min(entry.posts.posts.count, (widgetFamily == .systemMedium ? 2 : 5)))) { post in
+                        PostView(post: entry.posts.posts[post], redacted: false)
+                    }
+                }
+                Spacer()
+            }.frame(maxHeight: .infinity).background(Color(UIImage(data: entry.imageData)?.averageColor ?? getSchemeColor() )
+                                                        .opacity(0.8))
+        }
+    }
+
+    func getSchemeColor() -> UIColor {
+        return colorScheme == .light ? .white : .black
+    }
+}
+
+// MARK: SwiftUI Previews
+
+struct Hot_Posts_Previews: PreviewProvider {
+    static var previews: some View {
+        Hot_PostsEntryView(entry: SubredditWithPosts(date: Date(), subreddit: "redacted", posts: SubredditPosts(date: Date(), subreddit: "redacted", posts: getBlankPosts()), imageData: getPreviewData()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+
+    static func getBlankPosts() -> [Post] {
+        return [Post(id: UUID().uuidString, author: "ccrama", subreddit: "slide_ios", title: "Slide is awesome!", image: "", date: 0, imageData: Data()), Post(id: UUID().uuidString, author: "ccrama", subreddit: "slide_ios", title: "Slide is awesome!", image: "", date: 0, imageData: Data())]
+    }
+
+    /*
+    Corresponds to USR_DOMAIN in info.plist, which derives its value
+    from USR_DOMAIN in the pbxproj build settings. Default is `ccrama.me`.
+    */
+    static func USR_DOMAIN() -> String {
+       return Bundle.main.object(forInfoDictionaryKey: "USR_DOMAIN") as! String
+    }
+
+    static func getPreviewData() -> Data {
+        var imageData: Data?
+        let shared = UserDefaults(suiteName: "group.\(USR_DOMAIN()).redditslide.prefs")
+        if let data = shared?.data(forKey: "rawall") {
+            imageData = data
+        }
+        return imageData ?? Data()
+    }
+
+}
+
+// MARK: - Views
+
+struct PostView: View {
+    var post: Post
+    var redacted: Bool
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    var body: some View {
+        HStack(alignment: .top) {
+            Link(destination: URL(string: "slide://redd.it/\(post.id)")!) {
+                VStack(alignment: .leading) {
+                    HStack {
+                        if redacted {
+                            Text(post.subreddit).font(.caption2).bold().foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.6).redacted(reason: .placeholder).alignmentGuide(.leading) { d in d[.leading] }
+                            Text(post.author).font(.caption2).foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.6).redacted(reason: .placeholder).alignmentGuide(.leading) { d in d[.leading] }
+                        } else {
+                            Text(post.subreddit).font(.caption2).bold().foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.6).alignmentGuide(.leading) { d in d[.leading] }
+                            Text(post.author).font(.caption2).foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.6).alignmentGuide(.leading) { d in d[.leading] }
+                        }
+                    }
+                    if redacted {
+                        Text(post.title).font(.footnote).bold().multilineTextAlignment(.leading).redacted(reason: .placeholder)
+                    } else {
+                        Text(post.title).font(.footnote).bold().multilineTextAlignment(.leading)
+                    }
+                }.padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 4))
+            }
+            Spacer()
+            if redacted {
+                Image(uiImage: UIImage(data: post.imageData) ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .cornerRadius(CGFloat(5))
+                    .clipped().padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)).redacted(reason: .placeholder).alignmentGuide(.trailing) { d in d[.trailing] }
+            } else {
+                Image(uiImage: UIImage(data: post.imageData) ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .cornerRadius(CGFloat(5))
+                    .clipped().padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)).alignmentGuide(.trailing) { d in d[.trailing] }
+            }
+        }
+    }
+}
+
+struct SubredditViewHorizontal: View {
+    var imageData: Data
+    var title: String
+    var redacted: Bool
+
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
+    var body: some View {
+        HStack {
+            if redacted {
+                Image(uiImage: UIImage(data: imageData) ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .clipShape(Circle())
+                    .clipped().redacted(reason: .placeholder)
+                Text(self.title).font(.headline).bold().foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.8).redacted(reason: .placeholder).alignmentGuide(.leading) { d in d[.leading] }
+            } else {
+                Image(uiImage: UIImage(data: imageData) ?? UIImage())
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .clipShape(Circle())
+                    .clipped()
+                Text(self.title).font(.headline).bold().foregroundColor(colorScheme == .light ? .primary : .white).opacity(0.8).alignmentGuide(.leading) { d in d[.leading] }
+            }
+        }.widgetURL(URL(string: "slide://www.reddit.com/r/\(title)")).padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+    }
+}
