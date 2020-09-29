6feceb3690fc461c172e51884aaaad74c30cc63f
Josiah Agosto <Agosto_Josiah@yahoo.com>
2020-09-29 13:46:45 -0400

    Initial Commit

---
+++ Slide for Reddit/Networking/FallbackNetworkMonitor.swift
@@ -0,0 +1,14 @@
+//
+//  FallbackNetworkMonitor.swift
+//  Slide for Reddit
+//
+//  Created by Josiah Agosto on 9/29/20.
+//  Copyright © 2020 Haptic Apps. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class FallbackNetworkMonitor {
+    
+}
