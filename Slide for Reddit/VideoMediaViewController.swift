4125239cb344646c0e1bf1a91a594d8ca4877e47
ccrama <hapticappsdev@gmail.com>
2020-10-20 01:10:48 -0500

    Fixed audio downloads on VideoMediaVC

--- Slide for Reddit/VideoMediaViewController.swift
+++ Slide for Reddit/VideoMediaViewController.swift
@@ -573,7 +573,13 @@ class VideoMediaViewController: EmbeddableMediaViewController, UIGestureRecogniz
     
     func testQuality(urlToLoad: String, quality: String, completion: @escaping(_ success: Bool, _ url: String) -> Void) {
         Alamofire.request(urlToLoad.replacingOccurrences(of: "HLSPlaylist.m3u8", with: "DASH_\(quality)"), method: .get).responseString { response in
-            completion(response.response?.statusCode ?? 0 == 200, urlToLoad.replacingOccurrences(of: "HLSPlaylist.m3u8", with: "DASH_\(quality)"))
+            if response.response?.statusCode == 200 {
+                completion(response.response?.statusCode ?? 0 == 200, urlToLoad.replacingOccurrences(of: "HLSPlaylist.m3u8", with: "DASH_\(quality)"))
+            } else {
+                Alamofire.request(urlToLoad.replacingOccurrences(of: "HLSPlaylist.m3u8", with: "DASH_\(quality).mp4"), method: .get).responseString { response in
+                    completion(response.response?.statusCode ?? 0 == 200, urlToLoad.replacingOccurrences(of: "HLSPlaylist.m3u8", with: "DASH_\(quality).mp4"))
+                }
+            }
         }
     }
 
@@ -638,7 +644,7 @@ class VideoMediaViewController: EmbeddableMediaViewController, UIGestureRecogniz
         let key = getKeyFromURL()
         var toLoadAudio = self.data.baseURL!.absoluteString
         toLoadAudio = toLoadAudio.substring(0, length: toLoadAudio.lastIndexOf("/") ?? toLoadAudio.length)
-        toLoadAudio += "/audio"
+        toLoadAudio += "/DASH_audio.mp4"
         let finalUrl = URL.init(fileURLWithPath: key)
         let localUrlV = URL.init(fileURLWithPath: videoLocation)
         let localUrlAudio = URL.init(fileURLWithPath: key.replacingOccurrences(of: ".mp4", with: "audio.mp4"))
