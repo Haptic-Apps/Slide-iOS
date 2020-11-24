873bcc86fcba3efc06c052f1682198543aa740e5
Josiah Agosto <Agosto_Josiah@yahoo.com>
2020-11-24 01:56:17 -0500

    Update CommentViewController+UIGestureRecognizerDelegate.swift

--- Slide for Reddit/Comments/Extensions/CommentViewController+UIGestureRecognizerDelegate.swift
+++ Slide for Reddit/Comments/Extensions/CommentViewController+UIGestureRecognizerDelegate.swift
@@ -23,6 +23,7 @@ extension CommentViewController: UIGestureRecognizerDelegate {
         }
         if let nav = self.navigationController as? SwipeForwardNavigationController {
             nav.fullWidthBackGestureRecognizer.require(toFail: cellGestureRecognizer)
+            nav.interactivePushGestureRecognizer?.require(toFail: cellGestureRecognizer)
             if let interactivePop = nav.interactivePopGestureRecognizer {
                 cellGestureRecognizer.require(toFail: interactivePop)
             }
@@ -41,7 +42,9 @@ extension CommentViewController: UIGestureRecognizerDelegate {
             }
         }
         if SettingValues.commentGesturesMode == .FULL {
-            //setupFullSwipeView(self.tableView.tableHeaderView)
+            if let full = fullWidthBackGestureRecognizer {
+                full.view?.removeGestureRecognizer(full)
+            }
             return
         }
         
@@ -51,7 +54,7 @@ extension CommentViewController: UIGestureRecognizerDelegate {
     }
     
     func setupFullSwipeView(_ view: UIView?) {
-        if shouldSetupSwipe == false {
+        if shouldSetupSwipe == false || SettingValues.commentGesturesMode == .FULL {
             return
         }
         if let full = fullWidthBackGestureRecognizer {
@@ -60,24 +63,40 @@ extension CommentViewController: UIGestureRecognizerDelegate {
 
         fullWidthBackGestureRecognizer = UIPanGestureRecognizer()
         if let interactivePopGestureRecognizer = parent?.navigationController?.interactivePopGestureRecognizer, let targets = interactivePopGestureRecognizer.value(forKey: "targets"), parent is ColorMuxPagingViewController, !swipeBackAdded {
-            fullWidthBackGestureRecognizer.require(toFail: tableView.panGestureRecognizer)
-            if let navGesture = self.navigationController?.interactivePopGestureRecognizer {
-                fullWidthBackGestureRecognizer.require(toFail: navGesture)
+            setupSwipeWithTarget(fullWidthBackGestureRecognizer, interactivePopGestureRecognizer: interactivePopGestureRecognizer, targets: targets)
+        } else if !(parent is ColorMuxPagingViewController) && !swipeBackAdded {
+            if let interactivePopGestureRecognizer = self.navigationController?.interactivePopGestureRecognizer, let targets = interactivePopGestureRecognizer.value(forKey: "targets") {
+                setupSwipeWithTarget(fullWidthBackGestureRecognizer, interactivePopGestureRecognizer: interactivePopGestureRecognizer, targets: targets)
             }
-            fullWidthBackGestureRecognizer.require(toFail: interactivePopGestureRecognizer)
-            for view in parent?.view.subviews ?? [] {
-                if view is UIScrollView {
-                    (view as! UIScrollView).panGestureRecognizer.require(toFail: fullWidthBackGestureRecognizer)
-                }
-            }
+        }
+        if let nav = navigationController as? SwipeForwardNavigationController {
+            let gesture = nav.fullWidthBackGestureRecognizer
+            nav.interactivePushGestureRecognizer?.require(toFail: fullWidthBackGestureRecognizer)
+            gesture.require(toFail: fullWidthBackGestureRecognizer)
+        }
+    }
 
-            fullWidthBackGestureRecognizer.setValue(targets, forKey: "targets")
-            fullWidthBackGestureRecognizer.delegate = self
-            //parent.requireFailureOf(fullWidthBackGestureRecognizer)
-            view?.addGestureRecognizer(fullWidthBackGestureRecognizer)
-            if #available(iOS 13.4, *) {
-                fullWidthBackGestureRecognizer.allowedScrollTypesMask = .continuous
+    func setupSwipeWithTarget(_ fullWidthBackGestureRecognizer: UIPanGestureRecognizer, interactivePopGestureRecognizer: UIGestureRecognizer, targets: Any?) {
+        fullWidthBackGestureRecognizer.require(toFail: tableView.panGestureRecognizer)
+        if let navGesture = self.navigationController?.interactivePopGestureRecognizer {
+            fullWidthBackGestureRecognizer.require(toFail: navGesture)
+        }
+        if let navGesture = (self.navigationController as? SwipeForwardNavigationController)?.fullWidthBackGestureRecognizer {
+            navGesture.require(toFail: fullWidthBackGestureRecognizer)
+        }
+        fullWidthBackGestureRecognizer.require(toFail: interactivePopGestureRecognizer)
+        for view in parent?.view.subviews ?? [] {
+            if view is UIScrollView {
+                (view as! UIScrollView).panGestureRecognizer.require(toFail: fullWidthBackGestureRecognizer)
             }
+        }
+
+        fullWidthBackGestureRecognizer.setValue(targets, forKey: "targets")
+        fullWidthBackGestureRecognizer.delegate = self
+        //parent.requireFailureOf(fullWidthBackGestureRecognizer)
+        view?.addGestureRecognizer(fullWidthBackGestureRecognizer)
+        if #available(iOS 13.4, *) {
+            fullWidthBackGestureRecognizer.allowedScrollTypesMask = .continuous
         }
     }
 
@@ -93,10 +112,10 @@ extension CommentViewController: UIGestureRecognizerDelegate {
                     return false
                 }
                 if translation.x < 0 {
-                    if gestureRecognizer.location(in: tableView).x > tableView.frame.width * 0.5 || SettingValues.commentGesturesMode == .FULL {
+                    if gestureRecognizer.location(in: tableView).x > tableView.frame.width * 0.5 || !SettingValues.commentGesturesMode.shouldPage() {
                         return true
                     }
-                } else if SettingValues.commentGesturesMode == .FULL && abs(translation.x) > abs(translation.y) {
+                } else if !SettingValues.commentGesturesMode.shouldPage() && abs(translation.x) > abs(translation.y) {
                     return gestureRecognizer.location(in: tableView).x > tableView.frame.width * 0.1
                 }
                 return false
@@ -114,7 +133,7 @@ extension CommentViewController: UIGestureRecognizerDelegate {
         if recognizer.view != nil {
             let velocity = recognizer.velocity(in: recognizer.view!)
 
-            if (velocity.x < 0 && (SettingValues.commentActionLeftLeft == .NONE && SettingValues.commentActionLeftRight == .NONE) && translatingCell == nil) || (velocity.x > 0 && (SettingValues.commentGesturesMode == .HALF ||  (SettingValues.commentActionRightLeft == .NONE && SettingValues.commentActionRightRight == .NONE)) && translatingCell == nil) {
+            if (velocity.x < 0 && (SettingValues.commentActionLeftLeft == .NONE && SettingValues.commentActionLeftRight == .NONE) && translatingCell == nil) || (velocity.x > 0 && (SettingValues.commentGesturesMode == .HALF || SettingValues.commentGesturesMode == .HALF_FULL || (SettingValues.commentActionRightLeft == .NONE && SettingValues.commentActionRightRight == .NONE)) && translatingCell == nil) {
                 return
             }
         }
@@ -146,5 +165,5 @@ extension CommentViewController: UIGestureRecognizerDelegate {
             enableDismissalRecognizers()
         }
     }
-    
+
 }
