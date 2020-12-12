b8afe8fa4e7e878cb3a0d4677bfc6dca2cdf7afe
Carlos Crane <hapticappsdev@gmail.com>
2020-12-12 12:17:59 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ fi
 if [ -e "Gemfile" ]; then
   echo "installing ruby gems";
   # install bundler gem for ruby dependency management
-  gem install bundler --no-document || echo "failed to install bundle";
+  gem install bundler:1.17.3 --no-document || echo "failed to install bundle";
   bundle config set deployment 'true';
   bundle config path vendor/bundle;
   bundle install || echo "failed to install bundle";
