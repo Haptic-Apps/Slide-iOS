d10770f49f64777327a58bb522096dfb36e2004b
Carlos Crane <hapticappsdev@gmail.com>
2020-12-12 13:10:48 -0600

    Install Danger

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,6 +16,8 @@ if [ -e "Gemfile" ]; then
   # install bundler gem for ruby dependency management
   gem uninstall bundler
   gem install bundler:1.17.3 --no-document || echo "failed to install bundle"; #1.17.3 is needed for other deps
+  gem install danger --no-document || echo "failed to install danger";
+  
   bundle config set deployment 'true';
   bundle config path vendor/bundle;
   bundle install || echo "failed to install bundle";
